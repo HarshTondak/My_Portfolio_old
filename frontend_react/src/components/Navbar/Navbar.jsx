0cfcae03f3e88ad7738899209fc996296fb83993
Harsh Tondak <96473066+HarshTondak@users.noreply.github.com>
2022-06-04 22:30:02 +0530

    Update Navbar.jsx

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -48,30 +48,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
-
-
-
-// import React from 'react'
-// import './Navbar.scss'
-// import {images} from '../../constants'
-
-// const Navbar = () => {
-//   return (
-//     <nav classname="app__navbar">
-//       <div classname="app__navbar-logo">
-//         <img src={images.logo} alt="logo" />
-//       </div>
-//       <ul classname="app__navbar-links">
-//         {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
-//           <li classname="app__flex p-text" key={`link-${item}`}>
-//             <div/>
-//             <a href={`#${item}`}>{item}</a>
-//           </li>
-//         ))}
-//       </ul>
-//     </nav>
-//   )
-// }
-
-// export default Navbar
